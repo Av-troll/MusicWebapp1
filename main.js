8b3753d44edd808fb768d9ce7df373e80456c911
AvniBookworm4ever <79648896+Av-troll@users.noreply.github.com>
2022-01-02 09:46:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 song1 = "";
 song2 = "";
+
+var status_song1 = "";
+var status_song2 = "";
+
+var scoreLeftWrist = 0;
+var scorerightWrist = 0;
+
 var leftWristX = 0;
 var leftWristY = 0;
 var rightWristX = 0;
@@ -14,6 +21,7 @@ function setup(){
 
     var poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on("pose", gotPoses);
+
 }
 
 function modelLoaded(){
@@ -22,17 +30,38 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,600,500);
+    fill("red");
+    stroke("red");
+    status_song1 = song1.isPlaying();
+    status_song2 = song2.isPlaying();
+
+
+     if(scorerightWrist >0.2){
+         circle(rightWristX,rightWristY,20);
+         song2.stop();
+
+         if(status_song1 == false){
+             song1.play();
+             document.getElementById("song").innerHTML = "Playing - Hedwigs Harry Potter Theme"
+         }
+     }
+
+     if(scoreLeftWrist >0.2){
+        circle(rightWristX,rightWristY,20);
+        song1.stop();
+
+        if(status_song2 == false){
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing - Fireflies";
+        }
+    }
+
+ 
 }
 
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("Owl_City_Fireflies.mp3");
-    song1.setVolume(1);
-    song1.rate(1);
-    song1.setVolume(1);
-    
-    song2.setVolume(1);
-    song2.rate(1);
 }
 function gotPoses(results){
     if(results.length > 0){
@@ -40,6 +69,12 @@ function gotPoses(results){
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
+    
         rightWristY = results[0].pose.rightWrist.y;
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scorerightWrist = results[0].pose.keypoints[10].score;
     }
+
 }
+
